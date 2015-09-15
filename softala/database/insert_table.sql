546cb0b973a09f48e9872c4b9ccfbef144626fc8
a1400235 <a1400235@pao5009z07.haagahelia.amk>
2015-09-15 10:25:04 +0300

    lisääääää paikkoja

--- softala/database/insert_table.sql
+++ softala/database/insert_table.sql
@@ -3,6 +3,51 @@ INSERT INTO Paikka
 VALUES
 (5009, 999, 'taulutussit korvattu simpukoilla');
 
+INSERT INTO Paikka
+(paikka_id, hloMaara, lisatiedot)
+VALUES
+(5008, 888, 'taulutussit korvattu herkkusienillä');
+
+INSERT INTO Paikka
+(paikka_id, hloMaara, lisatiedot)
+VALUES
+(5007, 777, 'taulutussit korvattu katkaravuilla');
+
+INSERT INTO Paikka
+(paikka_id, hloMaara, lisatiedot)
+VALUES
+(5006, 666, 'taulutussit korvattu meduusoilla');
+
+INSERT INTO Paikka
+(paikka_id, hloMaara, lisatiedot)
+VALUES
+(5005, 555, 'taulutussit korvattu lonkeroilla');
+
+INSERT INTO Paikka
+(paikka_id, hloMaara, lisatiedot)
+VALUES
+(5004, 444, 'taulutussit korvattu merimakkaroilla');
+
+INSERT INTO Paikka
+(paikka_id, hloMaara, lisatiedot)
+VALUES
+(5003, 333, 'taulutussit korvattu mureenoilla');
+
+INSERT INTO Paikka
+(paikka_id, hloMaara, lisatiedot)
+VALUES
+(5002, 222, 'taulutussit korvattu trilobiiteilla');
+
+INSERT INTO Paikka
+(paikka_id, hloMaara, lisatiedot)
+VALUES
+(5001, 111, 'taulutussit korvattu planktoneilla');
+
+INSERT INTO Paikka
+(paikka_id, hloMaara, lisatiedot)
+VALUES
+(5000, 101, 'taulutussit korvattu sinivalailla');
+
 INSERT INTO Opettaja
 (etunimi, sukunimi, puhNro, email, lisatiedot)
 VALUES
