629d8c5ad281e9ffce9c4e20d4cc3f2fa6407ac5
a1400235 <a1400235@pao5009z07.haagahelia.amk>
2015-09-17 09:55:33 +0300

    Pikkujuttuja korjattu

--- softala/database/select_lauseita.sql
+++ softala/database/select_lauseita.sql
@@ -2,6 +2,6 @@ SELECT * FROM Koulutuksen_kouluttaja ko
 JOIN Koulutus k 
 ON ko.koulutus_id = k.koulutus_id 
 JOIN Kouluttaja kou 
-ON ko.kouluttaja_id = kou.kouluttaja_id
+ON ko.kouluttaja_id = kou.kouluttaja_id 
 JOIN Opettaja op 
-ON k.opettaja_id = op.opettaja_id
+ON k.opettaja_id = op.opettaja_id;
