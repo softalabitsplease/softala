7d4634d10e793aaf5228cbe7bfad75c75e8e043b
a1400235 <a1400235@pao5009z07.haagahelia.amk>
2015-09-17 09:49:29 +0300

    Yhdenmukaisuuksia korjattu

--- softala/database/insert_table.sql
+++ softala/database/insert_table.sql
@@ -59,27 +59,27 @@ VALUES
 ('Tuhka', 'Tomaattikastike', 050101337, 'tuhka.tomaattikastike@makaroni.com', 'Kouluttajan kohtalo on muuttua kastikkeeksi');
 
 INSERT INTO Koulutus
-(pvm, alkamisaika, paattymisaika, paikka, aihe, kuvaus, opettaja)
+(pvm, alkamisaika, paattymisaika, paikka_id, aihe, kuvaus, opettaja_id)
 VALUES
 ('2015-09-08', '13:37', '13:38', 5009, 'Katkarapujen koulutus', 'Luento kertoo syvällisesti katkarapujen sielunelämästä', 1);
 
 INSERT INTO Koulutus
-(pvm, alkamisaika, paattymisaika, paikka, aihe, kuvaus, opettaja)
+(pvm, alkamisaika, paattymisaika, paikka_id, aihe, kuvaus, opettaja_id)
 VALUES
 ('2014-10-27', '14:45', '15:15', 5009, 'Pizzanpyöritys', 'Opetellaan tekemään erilaisia pizzoja', 1);
 
 INSERT INTO Koulutus
-(pvm, alkamisaika, paattymisaika, paikka, aihe, kuvaus, opettaja)
+(pvm, alkamisaika, paattymisaika, paikka_id, aihe, kuvaus, opettaja_id)
 VALUES
 ('2018-03-22', '16:00', '18:00', 5009, 'Hajoamisen ensiasteet', 'Harjoitellaan Githubin käyttöä ja todennäköisesti hajotaan siihen', 1);
 
 INSERT INTO Koulutus
-(pvm, alkamisaika, paattymisaika, paikka, aihe, kuvaus, opettaja)
+(pvm, alkamisaika, paattymisaika, paikka_id, aihe, kuvaus, opettaja_id)
 VALUES
 ('2016-01-01', '10:00', '12:00', 5001, 'Päiväunien lyhyt oppimäärä', 'Pohditaan, mitä hyötyjä ja haittoja päiväunista on... ja ehkä nukutaan', 1);
 
 INSERT INTO Koulutus
-(pvm, alkamisaika, paattymisaika, paikka, aihe, kuvaus, opettaja)
+(pvm, alkamisaika, paattymisaika, paikka_id, aihe, kuvaus, opettaja_id)
 VALUES
 ('2016-02-14', '14:00', '18:00', 5001, 'Pussailun pitkä oppimäärä', 'Pussailun harjoittelua, ota mukaan oma parisi', 1);
 
