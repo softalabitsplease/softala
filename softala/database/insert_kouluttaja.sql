78aa711954b2c8c370b3eac165dc7cc55f67fdeb
a1400235 <a1400235@pao5009z07.haagahelia.amk>
2015-10-08 10:21:01 +0300

    Lisätty kouluttajia ja koulutuksen kouluttajia -Sanna

--- softala/database/insert_kouluttaja.sql
+++ softala/database/insert_kouluttaja.sql
@@ -2,3 +2,18 @@ INSERT INTO Kouluttaja
 (etunimi, sukunimi, puhNro, email, lisatiedot)
 VALUES
 ('Tuhka', 'Tomaattikastike', 050101337, 'tuhka.tomaattikastike@makaroni.com', 'Kouluttajan kohtalo on muuttua kastikkeeksi');
+
+INSERT INTO Kouluttaja
+(etunimi, sukunimi, puhNro, email, lisatiedot)
+VALUES
+('Sofia', 'Salejärvi', 050125945, 'sofia.salejarvi@myy.haaga-helia.fi', 'Kymmenkertainen Pokemonmestari');
+
+INSERT INTO Kouluttaja
+(etunimi, sukunimi, puhNro, email, lisatiedot)
+VALUES
+('Mikael', 'Salmela', 050367562, 'mikael.salmela@myy.haaga-helia.fi', 'Ei ole pokemonmestari');
+
+INSERT INTO Kouluttaja
+(etunimi, sukunimi, puhNro, email, lisatiedot)
+VALUES
+('Pietari', 'Tavela', 050456951, 'pietari.tavela@myy.haaga-helia.fi', 'Mahdollista lahjoa Charizardeilla');
--- softala/database/insert_koulutuksenkouluttaja.sql
+++ softala/database/insert_koulutuksenkouluttaja.sql
@@ -8,4 +8,22 @@ INSERT INTO Koulutuksen_kouluttaja
 VALUES
 (2, 1);
 
+INSERT INTO Koulutuksen_kouluttaja
+(koulutus_id, kouluttaja_id)
+VALUES
+(3, 2);
 
+INSERT INTO Koulutuksen_kouluttaja
+(koulutus_id, kouluttaja_id)
+VALUES
+(4, 3);
+
+INSERT INTO Koulutuksen_kouluttaja
+(koulutus_id, kouluttaja_id)
+VALUES
+(5, 4);
+
+INSERT INTO Koulutuksen_kouluttaja
+(koulutus_id, kouluttaja_id)
+VALUES
+(6, 4);
