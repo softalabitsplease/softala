a8f4efa4e254ac953ea22004537e7ae7c0e9c14a
a1400201 <a1400201@pao5009z06.haagahelia.amk>
2015-10-08 10:17:15 +0300

    - Sofia

--- softala/database/insert_opettaja.sql
+++ softala/database/insert_opettaja.sql
@@ -2,3 +2,23 @@ INSERT INTO Opettaja
 (etunimi, sukunimi, puhNro, email, lisatiedot)
 VALUES
 ('Katka', 'Rapunen', 0401337101, 'katka.rapunen@pitkaleka.com', 'Palkka maksetaan katkarapuina');
+
+INSERT INTO Opettaja
+(etunimi, sukunimi, puhNro, email, lisatiedot)
+VALUES
+('Pekka', 'Pouta', 0403849174, 'pekka.pouta@meili.com', 'Tekee p‰iv‰st‰ poutaisemman!');
+
+INSERT INTO Opettaja
+(etunimi, sukunimi, puhNro, email, lisatiedot)
+VALUES
+('Matti', 'Nukku', 0401938593, 'mattinukku@unimaailma.com', 'Puhuu unissaan.');
+
+INSERT INTO Opettaja
+(etunimi, sukunimi, puhNro, email, lisatiedot)
+VALUES
+('Milla', 'Malli', 0403391847, 'milla.malli@email.com', 'Palkka maksetaan meikkein‰.');
+
+INSERT INTO Opettaja
+(etunimi, sukunimi, puhNro, email, lisatiedot)
+VALUES
+('Ralf', 'R‰yh‰', 0405917486, 'ralf.rayha@mail.com', 'Varokaa r‰yh‰‰j‰‰.');
